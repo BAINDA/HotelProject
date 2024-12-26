201b185adb19efe8116fb2e6ce3aa3711b8a113b
Bainda <giorgibainda14@gmail.com>
2024-12-27 02:12:52 +0400

    fix: Update styles and implement SweetAlert2 in booked rooms component
    - Fixed styles in booked rooms component dashboard
    - Removed unused styles from booked rooms SCSS
    - Added SweetAlert2 library for modal windows
    - Implemented SweetAlert2 logic for confirmation modals

--- src/app/navigate-pages/booked-rooms/booked-rooms.component.ts
+++ src/app/navigate-pages/booked-rooms/booked-rooms.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../services/api.service';
 import { BookingResponse } from '../../interfaces/booking-interface';
 import { CurrencyPipe, DatePipe, NgClass, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-booked-rooms',
@@ -17,6 +18,35 @@ export class BookedRoomsComponent implements OnInit {
   searchTerm: string = '';
 
   constructor(private apiService: ApiService) {}
+
+  showModal() {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: 'Are you sure you want to cancel the booking?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, cancel it!',
+        cancelButtonText: 'No, keep it!',
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire({
+            title: 'Cancelled!',
+            text: 'Your booking has been cancelled.',
+            icon: 'success',
+          });
+        }
+      });
+  }
 
   getBookingDetails() {
     this.apiService.getBookings().subscribe((bookings: BookingResponse[]) => {
